c2256aa45401da889ab611fba7e0dec2f8d5ac87
David Hat <67667605+David-hat47@users.noreply.github.com>
2020-07-01 02:40:24 +0100

    Add files via upload
    today very easy to install all pkg in one click in termux

---
+++ davidpkg.sh
@@ -0,0 +1,117 @@
+printf "\e[1;31m Before installation Type this termux-setup-storage and click allow"
+sleep 8
+
+
+printf "\n"
+printf "\e[1;32mhi Hacker welcome to my tool for install all pkg in one click" 
+
+printf "\e[1;31m
+
+
+
+
+██████╗░░█████╗░██╗░░░██╗██╗██████╗░
+██╔══██╗██╔══██╗██║░░░██║██║██╔══██╗
+██║░░██║███████║╚██╗░██╔╝██║██║░░██║
+██║░░██║██╔══██║░╚████╔╝░██║██║░░██║
+██████╔╝██║░░██║░░╚██╔╝░░██║██████╔╝
+╚═════╝░╚═╝░░╚═╝░░░╚═╝░░░╚═╝╚═════╝░" 
+
+printf "\e[1;37m
+
+D̶a̶v̶i̶d̶ H̶a̶t̶ D̶a̶v̶i̶d̶ H̶a̶t̶ D̶a̶v̶i̶d̶ H̶a̶t̶  D̶a̶v̶i̶d̶ H̶a̶t̶
+█
+██████╗░██╗░░██╗░██████╗░install all pkg 
+██╔══██╗██║░██╔╝██╔════╝░
+██████╔╝█████═╝░██║░░██╗░in one click 
+██╔═══╝░██╔═██╗░██║░░╚██╗
+██║░░░░░██║░╚██╗╚██████╔╝
+╚═╝░░░░░╚═╝░░╚═╝░╚═════╝░"
+sleep 6
+printf "\n"
+
+printf "\e[1;37m" 
+
+
+pkg update && upgrade 
+pkg install git -y
+pkg install python -y
+pkg install python2 -y
+pkg install unzip -y
+pkg install nano -y
+pkg install cat -y
+pkg install curl -y
+pkg install wget -y
+pkg install w3m -y
+pkg install php -y
+pkg install nodejs -y
+apt install golang -y
+apt install host -y
+apt install havij -y
+pkg install figlet -y
+pkg install wget -y
+pkg install cfengine -y
+pkg install db -y
+pkg install netpb -y
+pkg install postgresql -y
+pkg install uftrace -y
+pkg install python -y
+pkg install python2 -y
+pkg install python3 -y
+pkg install java -y
+pkg install fish -y
+pkg install ruby -y
+pip install --upgrade pip
+pkg install host -y
+pkg install php -y
+pkg install perl -y
+pkg install nmap -y
+pkg install bash -y
+pkg install clang -y
+pkg install w3m -y
+pkg install havij -y
+pip install --upgrade pip
+pkg install figlet -y
+pkg install cowsay -y
+pkg install curl -y
+pkg install tar -y
+pkg install zip -y
+pkg install unzip -y
+pip install --upgrade pip
+pkg install tor -y
+pkg install google -y
+pkg install sudo -y
+pkg install wget -y
+pkg install wireshark -y
+pkg install wgetrc -y
+pkg install wcalc -y
+pkg install bmon -y
+pkg install unrar -y
+pkg install toilet -y
+pkg install proot -y
+pkg install net-tools -y
+pkg install golang -y
+pip install chroot -y
+pkg install openssl -y
+pkg install cmatrix -y
+pkg install openssh 
+pip install --upgrade pip 
+pkg install macchanger 
+pip install colorama 
+pip2 install mechanize 
+pip2 install requests 
+pip install --upgrade pip
+pkg install root-repo -y
+pkg install unstable-repo -y
+pkg install x11-repo -y
+apt update && apt upgrade -y
+
+printf "\e[1;32mtermux update finished
+
+
+Thank you for use my tool
+
+subscribe my channel >>David Hat<<"
+
+printf "\n"
+
